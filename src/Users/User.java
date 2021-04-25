b83fdc7c803b0186a310c6ac7cf53c38aa1ff1a9
Alex Udrea <alexandru.udrea@student.upt.ro>
2021-04-25 22:07:07 +0300

    User class

---
+++ src/Users/User.java
@@ -0,0 +1,56 @@
+class User
+{
+    public String username, password, phone_number, email;
+
+    public User(String username,String password,String phone_number,String email)
+    {
+        this.username=username;
+        this.password=password;
+        this.phone_number=phone_number;
+        this.email=email;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phone_number='" + phone_number + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
