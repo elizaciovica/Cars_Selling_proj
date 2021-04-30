886afcdc46c034c9bf73dcc31c1246f8d2b2085c
Eliza <elizaciovica0208@gmail.com>
2021-04-30 13:45:45 +0300

    Customer class

--- src/Users/Customer.java
+++ src/Users/Customer.java
@@ -1,9 +1,7 @@
 package Users;
-class Customer  extends User{
-        public Customer(String username, String password, String phone_number, String email)
-        {
-                super(username, password, phone_number, email);
+public class Customer extends User {
 
-        }
-
+    public Customer(String username, String password, String phone_number, String email) {
+        super(username, password, phone_number, email);
+    }
 }
