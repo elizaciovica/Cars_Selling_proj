695a97f7235e4d4340c94f772a503b4d5afcb822
Alex Udrea <alexandru.udrea@student.upt.ro>
2021-05-03 21:42:36 +0300

    Registration controller that works a little

--- src/Controllers/Registration.java
+++ src/Controllers/Registration.java
@@ -2,6 +2,9 @@ package Controllers;
 import database.Cryptography;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -9,7 +12,7 @@ import Exceptions.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Registration extends Application{
+public class Registration extends Application {
     @FXML
     private Text registrationMessage;
     @FXML
@@ -28,25 +31,50 @@ public class Registration extends Application{
     public TextField locationField;
     @FXML
     public TextField descriptionField;
+    @FXML
+    private ChoiceBox role;
 
-   public void handleRegisterAction() {
-       String username = usernameField.getText();
-       String mail = mailField.getText();
-       String address = addressField.getText();
-       String role = accountField.getText();
-       String phonenumber = phoneField.getText();
-       String location = locationField.getText();
-       String description = descriptionField.getText();
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Customer", "Manager");
+    }
 
-       String password = Cryptography.getMD5(passwordField.getText());
+    public void handleRegisterAction() {
+        String username = usernameField.getText();
+        String mail = mailField.getText();
+        String address = addressField.getText();
+        String role = accountField.getText();
+        String phonenumber = phoneField.getText();
+        String location = locationField.getText();
+        String description = descriptionField.getText();
 
-   }
+        String password = Cryptography.getMD5(passwordField.getText());
+
+    }
 
 
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../FXML/Register.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        // set up the stage
+        Stage stage = new Stage();
+        stage.setTitle("Registration");
+        stage.setWidth(800);
+        stage.setHeight(700);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
+
+
